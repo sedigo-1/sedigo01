98352fce1be3cbbd3d534894a8792b5c85797ff0
sedigo-1 <36073960+sedigo-1@users.noreply.github.com>
2018-04-03 04:29:19 -0700

    Add files via upload

---
+++ top-nav.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+<title>Frameset</title>
+<style type="text/css">
+body{
+font-family:verdana,arial,san-serif;
+fonst-size:10pt;
+margin:15px;
+background-color:#a08029;
+
+}
+</style>
+</head>
+<body>
+<h2>MENU</h2>
+
+<p>
+<a href="menu-1.html" target="menu">Minglanilla</a>
+| <a href="menu-2.html" target="menu">City of Naga</a>
+</p>
+</body> 
+</html>
