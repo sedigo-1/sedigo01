b0e2a532879fcd963c89a3d09d75f05325f33602
sedigo-1 <36073960+sedigo-1@users.noreply.github.com>
2018-04-03 04:36:42 -0700

    Update index.php

--- index.php
+++ index.php
@@ -4,13 +4,13 @@
 </head>
 <frameset rows="100,*,80" frameborder="0" border="0" framespacing="0">
 
-<frame name="topNav" src="top-nav.html">
+<frame name="topNav" src="top-nav.php">
 <frameset cols="200,*" frameborder="0" border="0" framespacing="0">
-<frame name="menu" src="menu-1.html" marginheight="0" marginwidth="0" scrolling="auto" noresize>
-<frame name="content" src="content.html" marginheight="0" marginwidth="0" scrolling="auto" noresize>
+<frame name="menu" src="menu-1.php" marginheight="0" marginwidth="0" scrolling="auto" noresize>
+<frame name="content" src="content.php" marginheight="0" marginwidth="0" scrolling="auto" noresize>
 </frameset>
 
-<frame name="footer" src="footer.html">
+<frame name="footer" src="footer.php">
 <noframes>
 </frameset>
 
