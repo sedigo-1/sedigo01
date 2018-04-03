3d9c8a6c70c271714c0e6ee5f0838632e82ddd10
sedigo-1 <36073960+sedigo-1@users.noreply.github.com>
2018-04-03 04:34:24 -0700

    Update dodgerblue.php

--- dodgerblue.php
+++ dodgerblue.php
@@ -20,7 +20,7 @@ background-color:blue;
 <p><b>AGE:</b><u>18</u></p>
 <p><b>SEX:</b><u>Male</u></p>
 <p><b>ADDRESS:</b><u>St. Joseph Village Tunghaan Minglanilla Cebu</u></p>
-<p> <a href="content.html" target="content"> back to the original</a>
+<p> <a href="content.php" target="content"> back to the original</a>
 </p>
 </body> 
 </html>
