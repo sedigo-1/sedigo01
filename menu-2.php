6a40b982cbf538be2b63f7de68df4adaa3fb5bfc
sedigo-1 <36073960+sedigo-1@users.noreply.github.com>
2018-04-03 04:38:32 -0700

    Update menu-2.php

--- menu-2.php
+++ menu-2.php
@@ -14,10 +14,10 @@ background-color:#7FFF00;
 <body>
 <h3>Friends List</h3>
 <ul>
-<li><p><a href="white.html" target="content">Jay-R Page</a></p>
-<li><p><a href="blue.html" target="content">Wilson Page</a></p>
-<li><p><a href="green.html" target="content">Benben Page</a></p>
-<li><p><a href="gray.html" target="content">Mc Kevent Page</a></p>
+<li><p><a href="white.php" target="content">Jay-R Page</a></p>
+<li><p><a href="blue.php" target="content">Wilson Page</a></p>
+<li><p><a href="green.php" target="content">Benben Page</a></p>
+<li><p><a href="gray.php" target="content">Mc Kevent Page</a></p>
 
 </ul>
 
