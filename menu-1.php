6e7cba7c80797b63b17b319d1f75cfea9bb876e3
sedigo-1 <36073960+sedigo-1@users.noreply.github.com>
2018-04-03 04:38:09 -0700

    Update menu-1.php

--- menu-1.php
+++ menu-1.php
@@ -15,12 +15,12 @@ background-color:#ff9900;
 <h3>Friends List</h3>
 <ul>
 
-<li><p><a href="lightgray.html" target="content">Albino page</a></p>
-<li><p><a href="tomato.html" target="content">John Benedict Page</a></p>
-<li><p><a href="dodgerblue.html" target="content">Micheal Page</a></p>
-<li><p><a href="violet.html" target="content">Nexon Page</a></p>
-<li><p><a href="crimson.html" target="content">Aljun Page</a></p>
-<li><p><a href="mediumseagreen.html" target="content">Hernel Page</a></p>
+<li><p><a href="lightgray.php" target="content">Albino page</a></p>
+<li><p><a href="tomato.php" target="content">John Benedict Page</a></p>
+<li><p><a href="dodgerblue.php" target="content">Micheal Page</a></p>
+<li><p><a href="violet.php" target="content">Nexon Page</a></p>
+<li><p><a href="crimson.php" target="content">Aljun Page</a></p>
+<li><p><a href="mediumseagreen.php" target="content">Hernel Page</a></p>
 </ul>
 
 </body> 
