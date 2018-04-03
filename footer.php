eb00b8c590fdca6d8d19a86b7c56c9308fb622f7
sedigo-1 <36073960+sedigo-1@users.noreply.github.com>
2018-04-03 04:35:13 -0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -18,16 +18,16 @@ color:White;
 <h5>You can also pick here .</h5>
 <p> 
 
-<a href="white.html" target="content">Jay-R Page</a> |
-<a href="blue.html" target="content">Wilson Page</a> |
-<a href="green.html" target="content">Benben Page</a> |
-<a href="gray.html" target="content">Mc Kevent Page</a> |
-<a href="lightgray.html" target="content">Albino page</a> |
-<a href="tomato.html" target="content">John Benedict Page</a> |
-<a href="dodgerblue.html" target="content">Micheal Page</a> |
-<a href="violet.html" target="content">Nexon Page</a> |
-<a href="crimson.html" target="content">Aljun Page</a> |
-<a href="mediumseagreen.html" target="content">Hernel Page</a>
+<a href="white.php" target="content">Jay-R Page</a> |
+<a href="blue.php" target="content">Wilson Page</a> |
+<a href="green.php" target="content">Benben Page</a> |
+<a href="gray.php" target="content">Mc Kevent Page</a> |
+<a href="lightgray.php" target="content">Albino page</a> |
+<a href="tomato.php" target="content">John Benedict Page</a> |
+<a href="dodgerblue.php" target="content">Micheal Page</a> |
+<a href="violet.php" target="content">Nexon Page</a> |
+<a href="crimson.php" target="content">Aljun Page</a> |
+<a href="mediumseagreen.php" target="content">Hernel Page</a>
 </p>
 </body> 
 </html>
